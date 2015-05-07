61660b796f916f8c0da229335cc9315bd6bad11a
Ryan VanSickle <ryan.vansickle@gmail.com>
2015-05-07 10:42:36 -0700

    update where account confirm redirects to?

--- app/auth/views.py
+++ app/auth/views.py
@@ -70,6 +70,7 @@ def confirm(token):
         return redirect(url_for('main.index'))
     if current_user.confirm(token):
         flash('You have confirmed your account. Thanks!')
+        return redirect(url_for('main.register_bike'))
     else:
         flash('The confirmation link is invalid or has expired.')
     return redirect(url_for('main.index'))
